3790edfe10d3bdcac0b750c412c054bed2f8172d
Thekra <th-f-a-a_1@hotmail.com>
2019-06-18 17:24:15 +0300

    Vote function

--- app/src/app/project-list/project-list.component.ts
+++ app/src/app/project-list/project-list.component.ts
@@ -7,12 +7,28 @@ import { IProject } from './Project';
   styleUrls: ['./project-list.component.css']
 })
 export class ProjectListComponent implements OnInit {
+  voteN: number = 0;
+  imgUrl: string = "img/NoVote2.png";
 
   projects: IProject[] = [];
+  rating: number;
+  starWidth: number;
+
+    ngOnChanges(): void {
+        this.starWidth = this.voteN * 75 / 5;
+      }
+
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  vote(): void {
+    console.log('vote');
+    this.voteN++;
+    console.log(this.voteN);
+    this.imgUrl = "img/Voted.png";
   }
 
 }
