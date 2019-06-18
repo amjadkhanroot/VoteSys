e16a6ecd13da74ea4126728a817787b3738baf23
amjadkhan1995 <amjad.khan.contact@gmail.com>
2019-06-18 14:27:59 +0300

    update
    ngbootstrap removed

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,8 +12,7 @@ import { ProjectListComponent } from './project-list/project-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    NgbModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
